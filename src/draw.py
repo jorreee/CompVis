eab33784850f5d4e0905f638798bb7e7a2d4a0a0
TimVDB <tim.vandenbroecke@student.kuleuven.be>
2017-08-04 10:28:19 +0200

    started draw rework

--- src/draw.py
+++ src/draw.py
@@ -9,8 +9,11 @@ import fnmatch
 import math
 
 lengthn = 160
-def draw_landmark(img,lpts,color=(255,0,0),thicc=1):
-    for ind in range(lengthn-1):
+# Takes a column vector and interprets it as all x coordinates followed by all y coordinates, then draws a line between the points on the given image
+def draw_contour(img,lm,color=(255,0,0),thicc=1):
+#    for ind in range(lengthn-1):
+	lpts = np.reshape(lm,((len/2) - 1,1),'F')
+    for ind in range(len(lpts)):
         if (ind+1) % 40 == 0:
             continue
         else:
@@ -20,27 +23,28 @@ def draw_landmark(img,lpts,color=(255,0,0),thicc=1):
                 color,
                 thicc);
     
-    # cv2.line(img,(int(float(lpts[lengthn-1,0])),int(float(lpts[lengthn-1,1]))),(int(float(lpts[0,0])),int(float(lpts[0,1]))),color,1);
-    
     return None
     
-def draw_all_landmarks(img,ls):
-    for lm in ls:
-        draw_landmark(img,lm)
+# Takes a matrix with landmarks as columns and draws them all on the given image
+def draw_all_contours(img,ls):
+    for lm in ls.T:
+        draw_landmark(img,lm.T)
     
     return None
-    
-def draw_aligned_landmarks(img,ls):
-    lss = ls
+	
+def convert_to_image_space(ls):
+    lss = np.copy(ls)
     for el in lss:
         for i in range(0,len(el)):
-            el[i,0] = el[i,0] * 503 * np.sqrt(lengthn/40) + 250
-            el[i,1] = el[i,1] * 503 * np.sqrt(lengthn/40) + 250
-    for i in range(len(lss)):
-        if i == 0:
-            draw_landmark(img,lss[i],color=(0,0,0),thicc=2)
-        else:
-            draw_landmark(img,lss[i],color=(200,200,0),thicc=2)
+            el[i,0] = el[i] * 503 + 250
+	return lss
+
+# Takes a matrix with aligned landmarks as column. Applies a scaling and translation transformation to them to be visible in image space and then draws the transformed landmarks on the given image
+def draw_aligned_contours(img,ls):
+	lst = np.copy(ls).T # alle lms zijn nu de rijen
+	for i in range(lst.shape[0]): # ga over alle rijen
+		lst[i] = convert_to_image_space(lst[i])
+    draw_all_contours(img,lst.T) # zet lms terug als columns en teken
     return None
 
 def draw_pca_reconstruction(img,lms):
