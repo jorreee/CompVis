ba19ffcf11074a5bae4c135e2e974e4c6f5df253
Joran VdW <joran.vdw@student.kuleuven.be>
2017-08-20 17:15:41 +0200

    Final method

--- src/fit.py
+++ src/fit.py
@@ -58,15 +58,15 @@ def find_new_points(imgtf, shapeo, edgeimgs, k, m):
 
 
 # Imgtf is enhanced image, edgeimgs are gradient images    
-def asm(imgtf, edgeimgs, b, tx, ty, s, theta, k, stdvar, mean, eigenvecs):
+def asm(imgtf, edgeimgs, b, tx, ty, s, theta, k, m, stdvar, mean, eigenvecs, maxiter):
     
-    for i in range(20):        
+    for i in range(maxiter):        
         print i
         col = io.greyscale_to_colour(imgtf)
         
         shapetf = lm.pca_reconstruct(b,mean,eigenvecs)
         shapetf = lm.transform_shape(shapetf, tx, ty, s, theta)        
-        approx, stop = find_new_points(imgtf, shapetf, edgeimgs, k, 2*k)
+        approx, stop = find_new_points(imgtf, shapetf, edgeimgs, k, m)
     
         draw.draw_contour(col,shapetf,(0,255,0))
         #draw.draw_contour(col,approx)
@@ -82,7 +82,7 @@ def asm(imgtf, edgeimgs, b, tx, ty, s, theta, k, stdvar, mean, eigenvecs):
         newshape = lm.pca_reconstruct(b,mean,eigenvecs)
         newshape = lm.transform_shape(newshape, tx, ty, s, theta)
         draw.draw_contour(col,newshape,(0,0,255))
-        io.show_on_screen(col,1)
+        #io.show_on_screen(col,1)
     
     return b, tx, ty, s, theta
     #result = lm.transform_shape(lm.pca_reconstruct(b,mean,eigenvecs),tx,ty,s,theta)
@@ -98,7 +98,7 @@ def asm(imgtf, edgeimgs, b, tx, ty, s, theta, k, stdvar, mean, eigenvecs):
     
     #return result
 
-def srasm2(enhimgtf, edgeimgs, marks, orient, k, modes):
+def srasm2(enhimgtf, edgeimgs, marks, orient, k, m, modes, maxiter):
     lms,_ = lm.procrustes(marks)
     mean, eigenvecs, eigenvals, lm_reduced = lm.pca_reduce(lms, modes)
     stdvar = np.std(lm_reduced, axis=1)
@@ -109,10 +109,10 @@ def srasm2(enhimgtf, edgeimgs, marks, orient, k, modes):
     imgtf = pp.apply_sobel(enhimgtf)
     imgtf = cv2.pyrDown(imgtf)
     edgies = np.array(map(lambda x: cv2.pyrDown(x),edgeimgs))
-    b, ntx, nty, nsc, itheta = asm(imgtf, edgies, b, itx / 2, ity / 2, isc / 2, itheta, k, stdvar, mean, eigenvecs)      
+    b, ntx, nty, nsc, itheta = asm(imgtf, edgies, b, itx / 2, ity / 2, isc / 2, itheta, k, m, stdvar, mean, eigenvecs, maxiter)      
     return lm.transform_shape(lm.pca_reconstruct(b,mean,eigenvecs),ntx * 2,nty * 2,nsc * 2,itheta)
 
-def srasm4(enhimgtf, edgeimgs, marks, orient, k, modes):
+def srasm4(enhimgtf, edgeimgs, marks, orient, k, m, modes, maxiter):
     lms,_ = lm.procrustes(marks)
     mean, eigenvecs, eigenvals, lm_reduced = lm.pca_reduce(lms, modes)
     stdvar = np.std(lm_reduced, axis=1)
@@ -123,10 +123,10 @@ def srasm4(enhimgtf, edgeimgs, marks, orient, k, modes):
     imgtf = pp.apply_sobel(enhimgtf)
     imgtf = cv2.pyrDown(cv2.pyrDown(imgtf))
     edgies = np.array(map(lambda x: cv2.pyrDown(cv2.pyrDown(x)),edgeimgs))
-    b, ntx, nty, nsc, itheta = asm(imgtf, edgies, b, itx / 4, ity / 4, isc / 4, itheta, k, stdvar, mean, eigenvecs)      
+    b, ntx, nty, nsc, itheta = asm(imgtf, edgies, b, itx / 4, ity / 4, isc / 4, itheta, k, m, stdvar, mean, eigenvecs, maxiter)      
     return lm.transform_shape(lm.pca_reconstruct(b,mean,eigenvecs),ntx * 4,nty * 4,nsc * 4,itheta)
     
-def srasm(enhimgtf, edgeimgs, marks, orient, k, modes):
+def srasm(enhimgtf, edgeimgs, marks, orient, k, m, modes, maxiter):
     lms,_ = lm.procrustes(marks)
     mean, eigenvecs, eigenvals, lm_reduced = lm.pca_reduce(lms, modes)
     stdvar = np.std(lm_reduced, axis=1)
@@ -135,10 +135,10 @@ def srasm(enhimgtf, edgeimgs, marks, orient, k, modes):
     b = np.zeros((modes,1))
     
     imgtf = pp.apply_sobel(enhimgtf)
-    b, ntx, nty, nsc, itheta = asm(imgtf, edgeimgs, b, itx, ity, isc, itheta, k, stdvar, mean, eigenvecs)      
+    b, ntx, nty, nsc, itheta = asm(imgtf, edgeimgs, b, itx, ity, isc, itheta, k, m, stdvar, mean, eigenvecs, maxiter)      
     return lm.transform_shape(lm.pca_reconstruct(b,mean,eigenvecs),ntx,nty,nsc,itheta)
 
-def mrasm(enhimgtf, edgeimgs, marks, orient, k, modes):
+def mrasm(enhimgtf, edgeimgs, marks, orient, k, m, modes, maxiter):
     lms,_ = lm.procrustes(marks)
     mean, eigenvecs, eigenvals, lm_reduced = lm.pca_reduce(lms, modes)
     stdvar = np.std(lm_reduced, axis=1)
@@ -156,24 +156,45 @@ def mrasm(enhimgtf, edgeimgs, marks, orient, k, modes):
             edgies = np.array(map(lambda x: cv2.pyrDown(x),edgies))
             j += 1 
         print edgies.shape
-        b, ntx, nty, nsc, itheta = asm(limgtf, edgies, b, itx / math.pow(2.0,(1-i)), ity / math.pow(2.0,(1-i)), isc / math.pow(2.0,(1-i)), itheta, k, stdvar, mean, eigenvecs)
+        b, ntx, nty, nsc, itheta = asm(limgtf, edgies, b, itx / math.pow(2.0,(1-i)), ity / math.pow(2.0,(1-i)), isc / math.pow(2.0,(1-i)), itheta, k, m, stdvar, mean, eigenvecs, maxiter)
         itx = ntx * math.pow(2.0,(1-i))
         ity = nty * math.pow(2.0,(1-i))
         isc = nsc * math.pow(2.0,(1-i))
         
     return lm.transform_shape(lm.pca_reconstruct(b,mean,eigenvecs),itx,ity,isc,itheta)
     
-    #colimgtf = io.greyscale_to_colour(imgtf) 
-    #draw.draw_contour(colimgtf,first,color=(0,255,0), thicc=1)
-    #io.show_on_screen(colimgtf,1)
-    #
-    #smalleritx = itx /2
-    #smallerity = ity /2
-    #smallerisc = isc /2
-    #smallershape = lm.transform_shape(mean,smalleritx,smallerity,smallerisc,itheta)
-    #smallercolimgtf = cv2.pyrDown(colimgtf)
-    #draw.draw_contour(smallercolimgtf,smallershape,color=(0,255,0), thicc=1)
-    #io.show_on_screen(smallercolimgtf,1)
+def match_image(imgind, orientation = 2, showground = True, modes = 5, k = 5, m = 10, maxiter = 50):
+    img = io.get_enhanced_img(imgind)
+    imges = io.get_all_gradient_img(imgind)
+    colgradimg = io.greyscale_to_colour(pp.apply_sobel(img))
+    
+    if orientation != 1:
+        marks = io.read_all_landmarks_by_orientation(0,imgind)
+        upper = mrasm(img, imges, marks,0, k, m, modes, maxiter)
+        draw.draw_contour(colgradimg,upper, thicc=1)
+        
+        if showground:
+            allmarks = io.read_all_landmarks_by_orientation(0)
+            upperground = np.reshape(allmarks[:,imgind - 1],(allmarks[:,imgind - 1].size,1))
+            upperground = lm.transform_shape(upperground,-1150,-500,1,0)
+            draw.draw_contour(colgradimg,upperground,color=(0,255,0), thicc=1)
+
+        
+    if orientation != 0:
+        marks = io.read_all_landmarks_by_orientation(1,imgind)
+        lower = mrasm(img, imges, marks,1, k, m, modes, maxiter)
+        draw.draw_contour(colgradimg,lower, thicc=1)
+        
+        if showground:
+            allmarks = io.read_all_landmarks_by_orientation(1)
+            lowerground = np.reshape(allmarks[:,imgind - 1],(allmarks[:,imgind - 1].size,1))
+            lowerground = lm.transform_shape(lowerground,-1150,-500,1,0)
+            draw.draw_contour(colgradimg,lowerground,color=(0,255,0), thicc=1)
+    
+          
+    io.show_on_screen(colgradimg,1)
+    return None
+    
 
 # Finds the shape parameter terms b and the pose transformation T(tx,ty,s,theta) 
 #  that best matches the model x = xbar + P * b (in object space) tot the new points Y (in image space).
@@ -211,7 +232,7 @@ def match_model_to_target(Y, xbar, P):
     
         #6
         lb = b
-        b = np.dot(P.T,(yprime - xbar)) 
+        b = np.dot(P.T,(yprime - xbar))
         if ((abs(b - lb) < conv_thresh).all() and abs(tx - ltx) < conv_thresh and 
             abs(ty - lty) < conv_thresh and abs(s - ls) < conv_thresh and abs(theta - ltheta) < conv_thresh):
             break;
@@ -219,74 +240,21 @@ def match_model_to_target(Y, xbar, P):
     return b, tx, ty, s, theta   
     
 if __name__ == '__main__':
-    wollah = io.get_enhanced_img(4)
-    imges = io.get_all_gradient_img(4)
-    marks = io.read_all_landmarks_by_orientation(0,4)
-    points = srasm2(wollah, imges, marks,0, 5, 5)
-    owollah = io.greyscale_to_colour(pp.apply_sobel(wollah))
-    
-    marks2 = io.read_all_landmarks_by_orientation(0)
-    ground = np.reshape(marks2[:,3],(marks2[:,3].size,1))
-    ground = lm.transform_shape(ground,-1150,-500,1,0)
-    draw.draw_contour(owollah,ground,color=(0,0,255), thicc=1)
-    
-    draw.draw_contour(owollah,points, thicc=1)
-    io.show_on_screen(owollah,1)
-    
-    
-    
-    #owollah = io.greyscale_to_colour(owollah)
-    #tx, ty, s, r = init.get_initial_transformation(wollah,mean,0)
-    #transformedmean = lm.transform_shape(mean,tx,ty,s,r)
-    #draw.draw_contour(owollah,points, thicc=2)
-    #io.show_on_screen(owollah,2)
+    match_image(2, orientation = 2, showground = True, modes = 5, k = 5, m = 10, maxiter = 20)
+    #wollah = io.get_enhanced_img(2)
+    #imges = io.get_all_gradient_img(2)
+    #marks = io.read_all_landmarks_by_orientation(0,2)
+    #points = srasm2(wollah, imges, marks,0, 5, 5)
+    #owollah = io.greyscale_to_colour(pp.apply_sobel(wollah))
+    #
+    #marks2 = io.read_all_landmarks_by_orientation(0)
+    #ground = np.reshape(marks2[:,1],(marks2[:,1].size,1))
+    #ground = lm.transform_shape(ground,-1150,-500,1,0)
+    #draw.draw_contour(owollah,ground,color=(0,0,255), thicc=1)
+    #
+    #draw.draw_contour(owollah,points, thicc=1)
+    #io.show_on_screen(owollah,1)
     
     
-    
-    
-    
-    #draw.draw_contour(wollah,transformedmean)
-    #io.show_on_screen_greyscale(wollah,2)
-    #lss = draw_initial_landmarks_orient(img_to_fit,[mean],0)
-    
-    ##singlemark = np.reshape(marks[:,0],(marks[:,0].size,1))
-    #normies = gl.get_normals(transformedmean)
-    #half = transformedmean.size /2
-    ###print (normies[0,0],normies[half,0])
-    #sliceu = gl.get_slice_indices(np.array([transformedmean[0,0],transformedmean[half,0]]),np.array([normies[0,0],normies[half,0]]),5)
-    #pmean, pcov = gl.get_statistical_model(imges,sliceu,5)
-    ##all_edge_imgs = io.get_all_enhanced_img(1)
-    
-    
-
-    # # Get image to fit
-    # img_to_fit = get_img(1)
-    # 
-    # y = get_jaw_separation(img_to_fit)
-    # draw_jaw_separation(img_to_fit,y)
-    # 
-    # # Get edge images to build statistical model from
-    # all_edge_imgs = get_all_edge_img(1)
-    # 
-    # # Get shape we'll try to fit
-    # lms = lm.read_all_landmarks_by_orientation(0)
-    # # Build ASM
-    # lms,meano = lm.procrustes(lms)
-    # mean, eigenvecs, eigenvals, lm_reduced = lm.pca_reduce(lms,meano,9)
-    # mean = mean.reshape(2,160).T
-    # lss = draw_initial_landmarks_orient(img_to_fit,[mean],0)
-    # 
-    # #init = draw_initial_landmarks(img_to_fit,[mean])
-    # 
-    # #img_to_fit = 
-    # newlmraw = fit(img_to_fit, lss, eigenvecs, all_edge_imgs, 5)
-    # 
-    # newlm = match_model_to_target(newlmraw, lss, eigenvecs)
-    # 
-    # lm.draw_landmark(img_to_fit,newlmraw)
-    # 
-    # #draw_initial_landmarks_orient(img_to_fit,[mean],0) 
-    # 
-    # #init = draw_initial_landmarks(img_to_fit,[mean])
 
     
